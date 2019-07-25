8f70d483f0033d643d904b2a2ef7352f6b1fc61d
jpz <jeanpierrezingraff@gmail.com>
2019-07-25 13:35:52 +0200

    Changed photos for design. Made the layout for ConnectionActivity and added specific buttons for Google and Facebook. Change the icon to launch the app.

--- app/src/main/java/com/jpz/go4lunch/activities/ConnectionActivity.java
+++ app/src/main/java/com/jpz/go4lunch/activities/ConnectionActivity.java
@@ -7,14 +7,12 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toolbar;
 
-import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.jpz.go4lunch.R;
 import com.jpz.go4lunch.utils.FirebaseUtils;
@@ -29,23 +27,28 @@ public class ConnectionActivity extends AppCompatActivity {
     private FirebaseUtils firebaseUtils = new FirebaseUtils();
 
     private ConstraintLayout constraintLayout;
+    private Button facebookLogin;
+    private SignInButton googleLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
 
-        constraintLayout = findViewById(R.id.connexion_activity_layout);
+        constraintLayout = findViewById(R.id.connection_activity_layout);
 
-        //Button button = findViewById(R.id.connexion_activity_button_login);
+        facebookLogin = findViewById(R.id.connection_activity_facebook_login);
+        googleLogin = findViewById(R.id.connection_activity_google_login);
+
 
         //startSignInActivity();
         Log.i("Tag", "user logged = " + firebaseUtils.isCurrentUserLogged());
-
+/*
         if (!firebaseUtils.isCurrentUserLogged())
             startSignInActivity();
         else
             startMainActivity();
+*/
     }
 
     @Override
@@ -64,7 +67,7 @@ public class ConnectionActivity extends AppCompatActivity {
                         new AuthUI.IdpConfig.GoogleBuilder().build()))
                 .setIsSmartLockEnabled(false, true)
                 //.setAuthMethodPickerLayout(customLayout)
-                .setTheme(R.style.AppTheme_Connection)
+                //.setTheme(R.style.AppTheme_Connection)
                 .build(), RC_SIGN_IN);
     }
 
@@ -110,8 +113,8 @@ public class ConnectionActivity extends AppCompatActivity {
     // that you have enabled.
     AuthMethodPickerLayout customLayout = new AuthMethodPickerLayout
             .Builder(R.layout.activity_connection)
-            .setFacebookButtonId(R.id.connexion_activity_facebook_login)
-            .setGoogleButtonId(R.id.connexion_activity_google_login)
+            .setFacebookButtonId(R.id.connection_activity_facebook_login)
+            .setGoogleButtonId(R.id.connection_activity_google_login)
             .build();
 */
 
