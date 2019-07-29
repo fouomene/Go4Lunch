341bfcbe90118bc8fb1d1550236cb2970316fd8a
jpz <jeanpierrezingraff@gmail.com>
2019-07-29 13:12:32 +0200

    Added Google API's for Maps and Location. Display Google Maps in MapFragment. Created the DetailsRestaurantActivity for future purpose.

--- app/src/main/java/com/jpz/go4lunch/fragments/MapFragment.java
+++ app/src/main/java/com/jpz/go4lunch/fragments/MapFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.jpz.go4lunch.R;
 
 
@@ -17,6 +18,10 @@ import com.jpz.go4lunch.R;
  * A simple {@link Fragment} subclass.
  */
 public class MapFragment extends Fragment {
+
+    private static View rootView;
+
+    private GoogleMap mMap;
 
 
     public MapFragment() {
@@ -28,7 +33,12 @@ public class MapFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        //return inflater.inflate(R.layout.fragment_map, container, false);
+
+        if (rootView == null)
+            rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        return rootView;
+
     }
 
 }
